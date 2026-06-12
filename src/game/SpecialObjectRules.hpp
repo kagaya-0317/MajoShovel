ea124f15af3623b393d9fbb21ba48c1cc194b535
kagaya-0317 <kagaya-0317@users.noreply.github.com>
2026-06-13 08:45:42 +0900

    Add special object drop rules

---
+++ src/game/SpecialObjectRules.hpp
@@ -0,0 +1,24 @@
+﻿#pragma once
+
+#include <string_view>
+
+namespace majo {
+
+inline constexpr std::string_view ApprenticeWitchStaffObjectId = "item_staff_apprentice_witch";
+
+inline bool isApprenticeWitchStaffObjectId(std::string_view objectId)
+{
+    return objectId == ApprenticeWitchStaffObjectId;
+}
+
+inline bool objectExcludedFromDungeonDrops(std::string_view objectId)
+{
+    return isApprenticeWitchStaffObjectId(objectId);
+}
+
+inline bool objectAllowedAsSpecialMerchantStock(std::string_view objectId)
+{
+    return isApprenticeWitchStaffObjectId(objectId);
+}
+
+}
